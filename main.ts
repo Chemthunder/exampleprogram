8a3633d5af836fe2fc996d827632472ee5b8772f
chemthunder <chemthunderisme@gmail.com>
2026-03-10 09:20:20 -0400

    Finalized

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 namespace Quottabyte {
-    export const QuottaCart = neptune.createCart("QuottaCart", _loadQuottaCart);
-    neptune.loadCart(Quottabyte.QuottaCart);
+    export const QuottaCart = neptune.createCart("QuottaCart", _loadQuottaCart); // creates a cartridge
+    neptune.loadCart(Quottabyte.QuottaCart); // loads a cartridge
     
     export function _loadQuottaCart() {
         const player = Foundation.buildPlayer(
@@ -22,24 +22,17 @@ namespace Quottabyte {
         ); // builds a player from the variables
 
 
-
-
-
-
-
-
-
-
         /* Plugin */
         let accessWidener = new accessor.Accessor(player); // transfers the 'player' variable to the Accessor
         neptune.bootstrapPlugin(pluginLoader(accessWidener)); // loads the plugin
     }
 
     export function pluginLoader(access: accessor.Accessor): neptune.Plugin {
-        const QuottaPlugin = neptune.createPlugin("QuottaPlugin", _loadQuottaPlugin);
+        const QuottaPlugin = neptune.createPlugin("QuottaPlugin", _loadQuottaPlugin); // creates a plugin
 
         function _loadQuottaPlugin() {
             const player = access.player; // allows usage of the player outside of the function it is registered in
+            player.vy = -120;
         }
 
         return QuottaPlugin;
